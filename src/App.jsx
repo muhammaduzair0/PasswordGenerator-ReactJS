cd93036466a71db9e2b10c6393290df557527d73
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-24 14:10:20 +0500

    Added Input Field

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 import { useState, useCallback } from "react";
 
 function App() {
@@ -23,17 +21,24 @@ function App() {
 
   return (
     <>
-    
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
         <h1 className="text-white text-center my-3">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
         </div>
         <div className="flex text-sm gap-x-2"></div>
       </div>
     </>
-  )
   );
 }
 
-export default App
 export default App;
