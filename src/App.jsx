4e144359c9401544cfd0005e135870111f6f7f2d
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-24 14:08:22 +0500

    Added div Container with Tailwind Classes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,16 @@ function App() {
   return (
     <>
     
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        </div>
+        <div className="flex text-sm gap-x-2"></div>
+      </div>
     </>
   )
+  );
 }
 
 export default App
+export default App;
