a3bf27db68d1e6b014018d5d49b4d5626592fdf9
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-24 14:09:16 +0500

    Added Function Using useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
+  const passwordGenerator = useCallback(() => {
 
+  }, [length, numAllowed, charAllowed, setPassword]);
 
   return (
     <>
