736fdf7c8d5d8dc2d71c6f20e2ec832533c2dd9e
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-04-26 13:57:02 +0500

    Added Reference to Input Field

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
           <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
             Copy
